75f49845aca89d383821338013c80e1f8424d13e
pangyuanxing <308106832@qq.com>
2014-10-17 18:23:53 +0800

    微信文章同步到网站上去

---
+++ casper/post.js
@@ -0,0 +1,56 @@
+var casper = require('casper').create({
+    pageSettings: {
+        userAgent : "chrome",
+        loadImages:  true,        // The WebPage instance used by Casper will
+        loadPlugins: false        // use these settings
+    }
+});
+
+
+var base64 =  require('../module/base64'),
+    fs = require('fs'),
+    postPath =  'loger/post_content.txt',
+    args2 = casper.cli.args,
+    post = JSON.parse(fs.read(postPath).toString());
+
+casper.start('http://www.baoxiaoyike.cn/wp-admin/post-new.php', function() {
+
+    this.fill('#loginform', {
+        'log': 'admin',
+        'pwd': 'yuan008598'
+    }, false);
+
+});
+
+casper.then(function() {
+    this.click('#wp-submit');
+    this.echo('login...');
+});
+
+
+
+casper.then(function() {
+    casper.evaluate(function(title, content, category) {
+        document.querySelector('#title').value = title;
+        if( typeof tinyMCE != 'undefined' ) {
+            tinyMCE.execCommand("mceInsertContent", false,  content);
+        } else {
+            document.querySelector('#content').innerHTML = content;
+        }
+
+        document.querySelector('#in-category-' + category).checked = true;
+
+        document.querySelector('#post-format-video').checked = true;
+
+        document.querySelector('#publish').click();
+
+    }, post.title, post.content, post["in-category"]);
+
+
+});
+
+casper.run(function() {
+    this.echo(JSON.stringify({
+        success : true
+    })).exit();
+});
--- routes/remote.js
+++ routes/remote.js
@@ -8,7 +8,7 @@ var nodegrass = require('nodegrass');
 var dateFormat = require('../module/dateFormat');
 var http = require("http");
 var fs = require('fs');
-var base64 =  require('./module/base64');
+var base64 =  require('../module/base64');
 var spawn = require('child_process').spawn;
 
 
@@ -17,10 +17,14 @@ exports.post = function(req, res){
 
         var logerDir = './loger',
             pageJson = logerDir + '/pageJson.txt',
-            resultPath = logerDir + '/result.txt';
+            resultPath = logerDir + '/result.txt',
+            postPath = logerDir + '/post_content.txt';
 
-    var pBody = req.body.data,
-        index = +req.body.index,
+    var pBody = req.body,
+        index = +pBody.index,
+        category = pBody["in-category"],
+        postState = pBody["post-state"],
+        have_post = pBody["have_post"],
         sendData = (function(){
             var obj = {
                   title : pBody.title
@@ -34,20 +38,47 @@ exports.post = function(req, res){
 
         }()),
 
+
+
         resultStr = fs.readFileSync(resultPath ).toString(),
         resultObj = JSON.parse( resultStr );
 
-    resultObj[index]["in-category"] = pBody["in-category"];
-    resultObj[index]["post-state"] = pBody["post-state"];
+    fs.writeFileSync(postPath, JSON.stringify(sendData) );
+
+    resultObj.list[index]["in-category"] = category;
+    resultObj.list[index]["post-state"] = postState;
+    //resultObj.list[index]["post-status"] = "发布成功";
+    //resultObj.list[index].success = 1;
 
     fs.writeFileSync(resultPath, JSON.stringify(resultObj) );
 
     res.set({'Content-Type':'text/plain'});
-    res.send(JSON.stringify({
-        success : true,
-        data : {
-            msg : "发布成功"
-        }
-    }));
+    if( postState == 1 && have_post == 0 ) {
+        var casper = spawn('casperjs', [
+            "casper/post.js"
+        ], {});
+
+        casper.stdout.on('data', function (data) {
+            data = data.toString();
+            console.log(data);
+        });
+
+        casper.on('exit', function (code,signal) {
+            res.send(JSON.stringify({
+                success : true,
+                data : {
+                    msg : "发布成功"
+                }
+            }));
+        });
+
+    } else {
+        res.send(JSON.stringify({
+            success : false,
+            data : {
+                msg : "发布成功"
+            }
+        }));
+    }
 
 };
